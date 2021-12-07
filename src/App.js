25d86bfb65081e00e89b5641408884525b9f3418
Danil <far.danil@outlook.com>
2021-12-07 22:46:11 +0300

    react files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EventTable from "./components/table/renderTable";
+import axios from "axios";
+import Context from "./context";
+import LoginForm from "./components/loginpage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [isUserAuthenticated, setIsUserAuthenticated] = useState(false)
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: '/api/is_auth/'
+        }).then(response => {
+            setIsUserAuthenticated(response.data.status)
+        })
+    },[])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: '/api/is_auth/'
+        }).then(response => {
+            setIsUserAuthenticated(response.data.status)
+        })
+    },[isUserAuthenticated])
+
+    function auth(value) {
+        setIsUserAuthenticated(value)
+    }
+
+    return (
+        <div className="col-md-12 col-lg-11 wrapper">
+            <Context.Provider value={{auth}}>
+            {isUserAuthenticated
+            ? <EventTable/>
+            : <LoginForm/>}
+            </Context.Provider>
+            {/*<iframe src="/static/media/silence.mp3" allow="autoplay" id="audio" style={{display: null}}/>*/}
+
+
+        </div>
+    );
 }
 
 export default App;
